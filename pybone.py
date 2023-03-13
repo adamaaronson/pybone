4ad1bb8cd730bbd7734bc544f3efe9d84b52afcd
Adam Aaronson <adamaaronson@gmail.com>
2023-03-13 13:46:50 -0500

    Implement position calculation

--- pybone.py
+++ pybone.py
@@ -19,32 +19,43 @@ class Note(Enum):
         return self.value
 
 SEMITONES_PER_OCTAVE = 12
+
 A440_NOTE = Note.A
 A440_OCTAVE = 4
 A440_HERTZ = 440
 A440_SEMITONES = A440_NOTE.get_semitones() + SEMITONES_PER_OCTAVE * A440_OCTAVE
 
 class Pitch:
-    def __init__(self, note: Note, octave: int):
+    def __init__(self, note: Note, octave: int, offset: float = 0):
         self.note = note
         self.octave = octave
+        self.offset = offset
 
-    def __str__(self):
-        return self.note.name + str(self.octave)
+    def __repr__(self):
+        if self.offset > 0:
+            offset_text = '+{0:.3g}'.format(self.offset)
+        elif self.offset < 0:
+            offset_text = '{0:.3g}'.format(self.offset)
+        else:
+            offset_text = ''
+        return self.note.name + str(self.octave) + offset_text
     
     def __eq__(self, other):
-        return self.note == other.note and self.octave == other.octave
+        return self.note == other.note and self.octave == other.octave and self.offset == other.offset
 
     def get_semitones(self):
-        semitones = self.note.get_semitones() + self.octave * SEMITONES_PER_OCTAVE
+        semitones = self.note.get_semitones() + self.octave * SEMITONES_PER_OCTAVE + self.offset
         return semitones - A440_SEMITONES
 
     @classmethod
     def from_semitones(cls, semitones: float):
-        note_semitones = round(semitones) + A440_SEMITONES
-        note = Note(note_semitones % SEMITONES_PER_OCTAVE)
-        octave = note_semitones // SEMITONES_PER_OCTAVE
-        return cls(note, octave)
+        note_semitones = semitones + A440_SEMITONES
+        rounded_semitones = round(note_semitones)
+
+        note = Note(rounded_semitones % SEMITONES_PER_OCTAVE)
+        octave = rounded_semitones // SEMITONES_PER_OCTAVE
+        offset = note_semitones - rounded_semitones
+        return Pitch(note, octave, offset)
     
     def get_hertz(self):
         return A440_HERTZ * 2 ** (self.get_semitones() / SEMITONES_PER_OCTAVE)
@@ -52,4 +63,54 @@ class Pitch:
     @classmethod
     def from_hertz(cls, hertz: float):
         semitones = log2(hertz / A440_HERTZ) * SEMITONES_PER_OCTAVE
-        return cls.from_semitones(semitones)
+        return Pitch.from_semitones(semitones)
+    
+    def remove_offset(self):
+        return Pitch(self.note, self.octave)
+
+
+TROMBONE_FUNDAMENTAL = Pitch(Note.Bb, 1)
+TROMBONE_SLIDE_LENGTH = 7.5
+
+
+class Trombone:
+    def __init__(self, fundamental: Pitch = TROMBONE_FUNDAMENTAL, slide_length: float = TROMBONE_SLIDE_LENGTH):
+        self.fundamental = fundamental
+        self.slide_length = slide_length
+    
+    def get_pitch(self, position: float, partial: int):
+        """
+        Returns the pitch played on a trombone in a given position and partial.
+        Positions are 0-indexed, i.e. 0 is first position, 1 is second position, etc.
+        Partials are 0-indexed from pedal, i.e. 0 is pedal Bb, 1 is Bb, 2 is F, etc.
+        """
+        semitones = self.fundamental.get_semitones() - position
+        position_pitch = Pitch.from_semitones(semitones)
+        position_pitch_hertz = position_pitch.get_hertz()
+        position_partial_pitch_hertz = position_pitch_hertz * (partial + 1)
+        position_partial_pitch = Pitch.from_hertz(position_partial_pitch_hertz)
+        return position_partial_pitch
+    
+    def get_position(self, pitch: Pitch, partial: int):
+        semitones = pitch.get_semitones()
+        first_position_hertz = self.fundamental.get_hertz()
+        first_position_partial_hertz = first_position_hertz * (partial + 1)
+        first_position_partial_pitch = Pitch.from_hertz(first_position_partial_hertz)
+        first_position_partial_semitones = first_position_partial_pitch.get_semitones()
+        return first_position_partial_semitones - semitones
+    
+    def get_all_positions(self, pitch: Pitch):
+        positions = []
+        partials = []
+        partial = 0
+        while True:
+            position = self.get_position(pitch, partial)
+            if position > self.slide_length:
+                # partial is too high to play the note
+                break
+            if position > -0.5: # allow flat notes in first position
+                positions.append(position)
+                partials.append(partial)
+            # otherwise, partial is too low to play the note yet
+            partial += 1
+        return positions, partials
--- test_pybone.py
+++ test_pybone.py
@@ -3,19 +3,78 @@ from pybone import *
 
 class TestGetHertz(unittest.TestCase):
     def runTest(self):
-        self.assertAlmostEqual(Pitch(Note.A, 4).get_hertz(), 440, places=2)
-        self.assertAlmostEqual(Pitch(Note.A, 5).get_hertz(), 880, places=2)
-        self.assertAlmostEqual(Pitch(Note.A, 3).get_hertz(), 220, places=2)
+        self.assertAlmostEqual(Pitch(Note.A, 4).get_hertz(), 440.00, places=2)
+        self.assertAlmostEqual(Pitch(Note.A, 5).get_hertz(), 880.00, places=2)
+        self.assertAlmostEqual(Pitch(Note.A, 3).get_hertz(), 220.00, places=2)
         self.assertAlmostEqual(Pitch(Note.B, 4).get_hertz(), 493.88, places=2)
         self.assertAlmostEqual(Pitch(Note.C, 5).get_hertz(), 523.25, places=2)
 
 class TestFromHertz(unittest.TestCase):
     def runTest(self):
-        self.assertEqual(Pitch.from_hertz(440), Pitch(Note.A, 4))
-        self.assertEqual(Pitch.from_hertz(880), Pitch(Note.A, 5))
-        self.assertEqual(Pitch.from_hertz(220), Pitch(Note.A, 3))
-        self.assertEqual(Pitch.from_hertz(493), Pitch(Note.B, 4))
-        self.assertEqual(Pitch.from_hertz(523), Pitch(Note.C, 5))
+        self.assertEqual(Pitch.from_hertz(440).remove_offset(), Pitch(Note.A, 4))
+        self.assertEqual(Pitch.from_hertz(880).remove_offset(), Pitch(Note.A, 5))
+        self.assertEqual(Pitch.from_hertz(220).remove_offset(), Pitch(Note.A, 3))
+        self.assertEqual(Pitch.from_hertz(493).remove_offset(), Pitch(Note.B, 4))
+        self.assertEqual(Pitch.from_hertz(523).remove_offset(), Pitch(Note.C, 5))
+
+class TestGetPitchFirstPosition(unittest.TestCase):
+    def runTest(self):
+        trombone = Trombone()
+        self.assertEqual(trombone.get_pitch(0, 0), Pitch(Note.Bb, 1))
+        self.assertEqual(trombone.get_pitch(0, 1), Pitch(Note.Bb, 2))
+        self.assertEqual(trombone.get_pitch(0, 2).remove_offset(), Pitch(Note.F, 3))
+        self.assertEqual(trombone.get_pitch(0, 3), Pitch(Note.Bb, 3))
+        self.assertEqual(trombone.get_pitch(0, 4).remove_offset(), Pitch(Note.D, 4))
+        self.assertEqual(trombone.get_pitch(0, 5).remove_offset(), Pitch(Note.F, 4))
+
+class TestGetPitchSecondPosition(unittest.TestCase):
+    def runTest(self):
+        trombone = Trombone()
+        self.assertEqual(trombone.get_pitch(1, 0), Pitch(Note.A, 1))
+        self.assertEqual(trombone.get_pitch(1, 1), Pitch(Note.A, 2))
+        self.assertEqual(trombone.get_pitch(1, 2).remove_offset(), Pitch(Note.E, 3))
+        self.assertEqual(trombone.get_pitch(1, 3), Pitch(Note.A, 3))
+        self.assertEqual(trombone.get_pitch(1, 4).remove_offset(), Pitch(Note.Db, 4))
+        self.assertEqual(trombone.get_pitch(1, 5).remove_offset(), Pitch(Note.E, 4))
+
+class TestGetPitchSeventhPosition(unittest.TestCase):
+    def runTest(self):
+        trombone = Trombone()
+        self.assertEqual(trombone.get_pitch(6, 0), Pitch(Note.E, 1))
+        self.assertEqual(trombone.get_pitch(6, 1), Pitch(Note.E, 2))
+        self.assertEqual(trombone.get_pitch(6, 2).remove_offset(), Pitch(Note.B, 2))
+        self.assertEqual(trombone.get_pitch(6, 3), Pitch(Note.E, 3))
+        self.assertEqual(trombone.get_pitch(6, 4).remove_offset(), Pitch(Note.Ab, 3))
+        self.assertEqual(trombone.get_pitch(6, 5).remove_offset(), Pitch(Note.B, 3))
+
+class TestGetPosition(unittest.TestCase):
+    def runTest(self):
+        trombone = Trombone()
+        self.assertEqual(trombone.get_position(Pitch(Note.Bb, 2), 1), 0)
+        self.assertAlmostEqual(trombone.get_position(Pitch(Note.F, 3), 2), 0, places=1)
+        self.assertAlmostEqual(trombone.get_position(Pitch(Note.C, 3), 2), 5, places=1)
+
+class TestAlternatePositions(unittest.TestCase):
+    def runTest(self):
+        trombone = Trombone()
+        self.assertAlmostEqual(trombone.get_position(Pitch(Note.D, 4), 4), 0, places=0)
+        self.assertAlmostEqual(trombone.get_position(Pitch(Note.D, 4), 5), 3, places=0)
+        self.assertAlmostEqual(trombone.get_position(Pitch(Note.F, 4), 5), 0, places=0)
+        self.assertAlmostEqual(trombone.get_position(Pitch(Note.F, 4), 6), 3, places=0)
+        self.assertAlmostEqual(trombone.get_position(Pitch(Note.F, 4), 7), 5, places=0)
+
+class TestGetAllPositions(unittest.TestCase):
+    def runTest(self):
+        trombone = Trombone()
+        positions, partials = trombone.get_all_positions(Pitch(Note.D, 4))
+        self.assertEqual(len(positions), 3)
+        self.assertAlmostEqual(positions[0], 0, places=0)
+        self.assertAlmostEqual(positions[1], 3, places=0)
+        self.assertAlmostEqual(positions[2], 6, places=0)
+        self.assertEqual(len(partials), 3)
+        self.assertEqual(partials[0], 4)
+        self.assertEqual(partials[1], 5)
+        self.assertEqual(partials[2], 6)
 
 
 if __name__ == '__main__':
